1b06e578bbc6d88c1805da5bf374b854aaba9f6e
carolardilae <142743220+carolardilae@users.noreply.github.com>
2023-09-06 09:40:09 -0500

    Add files via upload

---
+++ loops/Whileexample2.java
@@ -0,0 +1,13 @@
+
+package whileexample2;
+
+public class Whileexample2 {
+
+    public static void main(String[] args) {
+        while(true){
+        System.out.println("Infinite while loop");
+        }
+
+    }
+    
+}
