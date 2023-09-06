70426dc3489f1fead3c819ea539edde24be03b02
carolardilae <142743220+carolardilae@users.noreply.github.com>
2023-09-06 09:54:06 -0500

    Add files via upload

---
+++ loops/Dowhileexample.java
@@ -0,0 +1,13 @@
+
+package dowhileexample;
+
+public class Dowhileexample {
+
+    public static void main(String[] args) {
+       int i=1;
+       do{
+       System.out.println(i);
+       i++;
+       }while(i<=10);
+    }   
+}
