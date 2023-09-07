0eb522376c064406edf14d86c7d82516969ab7c4
carolardilae <142743220+carolardilae@users.noreply.github.com>
2023-09-07 09:36:53 -0500

    Add files via upload

---
+++ PrimerosProgramas/CheckingAccount.java
@@ -0,0 +1,13 @@
+
+package bank;
+
+public class CheckingAccount {
+    public String name;
+  public int balance;
+
+  public CheckingAccount(String inputName, int inputBalance){
+    name = inputName;
+    balance = inputBalance;
+  }
+}
+
