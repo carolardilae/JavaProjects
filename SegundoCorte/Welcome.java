f6df39452f473c6c8b3178ef3c8f5252cdc1abe6
carolardilae <142743220+carolardilae@users.noreply.github.com>
2023-10-05 09:47:01 -0500

    Add files via upload

---
+++ SegundoCorte/Welcome.java
@@ -0,0 +1,11 @@
+
+package welcome;
+import javax.swing.JOptionPane;
+
+public class Welcome {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Welcome\nto\nthe Java world");
+    }
+    
+}
