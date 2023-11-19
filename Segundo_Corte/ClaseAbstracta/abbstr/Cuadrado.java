0e090b57acf2fecef9b6ca7aaa12c0fb048949e3
carolardilae <142743220+carolardilae@users.noreply.github.com>
2023-11-19 17:27:04 -0500

    Add files via upload

---
+++ Segundo_Corte/ClaseAbstracta/abbstr/Cuadrado.java
@@ -0,0 +1,20 @@
+
+package abbstr;
+
+public class Cuadrado extends Figura {
+
+	private double lado;
+
+	public Cuadrado(String color, double lado) {
+		super(color);
+		this.lado = lado;
+	}
+
+
+	@Override
+	public double calcularArea() {
+		return lado*lado;
+	}	
+}
+
+
